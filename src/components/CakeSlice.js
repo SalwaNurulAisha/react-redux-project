da1b3679bb95ca6fae5dc9c89cf0363107efaea0
SalwaNurulAisha <salwa8l.25@gmail.com>
2022-07-04 21:45:42 +0700

    react-redux project

---
+++ src/components/CakeSlice.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createSlice } from "@reduxjs/toolkit/";
+
+const initialState = {
+  totalCakes: 99,
+};
+
+const cakeSlice = createSlice({
+  name: "cake",
+  initialState,
+
+  reducers: {
+    decrementCake: (state) => {
+      state.totalCakes--;
+    },
+
+    restockCake: (state) => {
+      state.totalCakes += 100;
+    },
+  },
+});
+
+export default cakeSlice.reducer;
+export const { decrementCake, restockCake } = cakeSlice.actions;
